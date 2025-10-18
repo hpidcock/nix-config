0677e9da9d0932772233dc7a558d3ad789780e61
Harry Pidcock <harry.pidcock@canonical.com>
2025-10-19 00:01:42 +1000

    chore: update flake

--- home/hpidcock-nixon/default.nix
+++ home/hpidcock-nixon/default.nix
@@ -1,19 +1,27 @@
 { inputs, ... }:
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable {
+    system = "x86_64-linux";
+    config = {
+      allowUnfree = true;
+    };
+  };
+in
 inputs.home-manager.lib.homeManagerConfiguration {
   pkgs = import inputs.nixpkgs {
     system = "x86_64-linux";
     config = {
       allowUnfree = true;
     };
+
+    overlays = [
+      (self: super: {
+        zed-editor = pkgs-unstable.zed-editor;
+      })
+    ];
   };
   modules = [ ./home.nix ];
   extraSpecialArgs = {
     inherit inputs;
-    pkgs-unstable = import inputs.nixpkgs-unstable {
-      system = "x86_64-linux";
-      config = {
-        allowUnfree = true;
-      };
-    };
   };
 }
