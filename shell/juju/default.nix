e9b35c7bbdbf2ffcbc5d75722775b73ae618a281
Harry Pidcock <harry.pidcock@canonical.com>
2025-10-26 05:18:42 +0100

    fix: allow unfree

--- shell/juju/default.nix
+++ shell/juju/default.nix
@@ -2,11 +2,17 @@
 let
   pkgs-unstable = import inputs.nixpkgs-unstable {
     inherit system;
+    config = {
+      allowUnfree = true;
+    };
   };
 in
 import ./shell.nix {
   pkgs = import inputs.nixpkgs {
     inherit system;
+    config = {
+      allowUnfree = true;
+    };
     overlays = [
       (final: prev: {
         go = pkgs-unstable.go;
