2009a38d5d74714cc3106ec7d7c717c8166b446c
Harry Pidcock <harry.pidcock@canonical.com>
2026-01-06 23:36:29 +1000

    fix: enable zsh

--- host/merkel/configuration.nix
+++ host/merkel/configuration.nix
@@ -56,6 +56,7 @@
     ];
     shell = pkgs.zsh;
   };
+  programs.zsh.enable = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
