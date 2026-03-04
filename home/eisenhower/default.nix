9b1d13eebaec76a3ef7210cefa183526989ae88f
Harry Pidcock <haza55@gmail.com>
2026-03-04 20:53:53 +1000

    fix: weirdness with keyring

--- home/eisenhower/default.nix
+++ home/eisenhower/default.nix
@@ -34,7 +34,16 @@ inputs.home-manager.lib.homeManagerConfiguration {
               rev = "9e0c5f42a9d41a1a47f168f9dc7403b4bc8b320f";
               hash = "sha256-JhdtzUa9s5C8CeEFH8Z3SOhR6i0JfXfceg6PPuzOqMY=";
             };
-            postPatch = builtins.replaceStrings [ prev.version ] [ final.version ] prev.postPatch;
+            postPatch = ''
+              # Dynamically link WebRTC instead of static
+              substituteInPlace $cargoDepsCopy/*/webrtc-sys-*/build.rs \
+                  --replace-fail "cargo:rustc-link-lib=static=webrtc" "cargo:rustc-link-lib=dylib=webrtc"
+            
+              # The generate-licenses script wants a specific version of cargo-about eventhough
+              # newer versions work just as well.
+              substituteInPlace script/generate-licenses \
+                  --replace-fail '$CARGO_ABOUT_VERSION' '${pkgs-unstable.cargo-about.version}'
+            '';
             cargoDeps =
               super.callPackage "${inputs.nixpkgs-staging}/pkgs/build-support/rust/fetch-cargo-vendor.nix"
                 { inherit (super) cargo; }
@@ -54,6 +63,14 @@ inputs.home-manager.lib.homeManagerConfiguration {
             };
           }
         );
+        signal-desktop = super.symlinkJoin {
+          name = "signal-desktop";
+          paths = [ super.signal-desktop ];
+          buildInputs = [ super.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/signal-desktop --add-flag '--password-store=gnome-libsecret'
+           '';
+        };
         ollama-vulkan = pkgs-unstable.ollama-vulkan;
         ollama = pkgs-unstable.ollama;
       })
