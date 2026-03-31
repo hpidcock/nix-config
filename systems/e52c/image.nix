25cce35e4c340133a045084e76da52b1039b38eb
Harry Pidcock <haza55@gmail.com>
2026-03-31 23:11:17 +1000

    feat: cleanup e52c

--- systems/e52c/image.nix
+++ systems/e52c/image.nix
@@ -1,23 +1,7 @@
 { inputs, ... }:
 let
-  pkgsHost = import inputs.nixpkgs-unstable {
-    system = "x86_64-linux";
-  };
-  pkgsCross = import inputs.nixpkgs-unstable {
-    crossSystem = "aarch64-linux";
-    config = {
-      allowUnfree = true;
-    };
-  };
   pkgs = import inputs.nixpkgs-unstable {
     system = "aarch64-linux";
-    overlays = [
-      (final: prev: {
-        qemu_kvm = pkgsHost.qemu;
-        lkl = pkgsHost.lkl;
-        linuxPackages_latest = pkgsCross.linuxPackages_latest;
-      })
-    ];
     config = {
       allowUnfree = true;
     };
